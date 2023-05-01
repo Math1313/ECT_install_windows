a3b4a93915d6e968b9bb460fd17cb34d840c656f
Math1313 <mathisgauthier@hotmail.com>
2023-05-01 12:29:48 -0400

    20230501.01
    Optimisation et point de restauration

--- installWindows.ps1
+++ installWindows.ps1
@@ -420,6 +420,15 @@ function CleanDesktop {
 # ------------------------------------------------------------------------------------------------------------------ #
 # ------------------------------------------------------------------------------------------------------------------ #
 
+Function OptimizeAndRestorePoint{
+    Optimize-Volume -DriveLetter C
+    Enable-ComputerRestore -Drive "C:"
+    vssadmin resize shadowstorage /for=C: /on=C: /maxsize=10GB
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" -Name "SystemRestorePointCreationFrequency" -Value 0 -PropertyType DWORD -Force
+    Checkpoint-Computer -Description "1"
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" -Name "SystemRestorePointCreationFrequency" -Force
+}
+
 Function EndOfScript
 {
     $window = New-Object -ComObject Wscript.Shell
@@ -470,6 +479,8 @@ SetDefaultWebBrowser
 SetDefaultPDFReader
 
 CleanDesktop
+
+OptimizeAndRestorePoint
 
 EndOfScript
 
--- test.ps1
+++ test.ps1
@@ -1,2 +1,6 @@
-$DesktopPath = [Environment]::GetFolderPath("Desktop")
-Write-Host "$DesktopPath"
+Optimize-Volume -DriveLetter C
+Enable-ComputerRestore -Drive "C:"
+vssadmin resize shadowstorage /for=C: /on=C: /maxsize=10GB
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" -Name "SystemRestorePointCreationFrequency" -Value 0 -PropertyType DWORD -Force
+Checkpoint-Computer -Description "1"
+Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SystemRestore" -Name "SystemRestorePointCreationFrequency" -Force
