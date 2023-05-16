e816f668045bd223854dcf7daf0b61079e6bdb61
Math1313 <mathisgauthier@hotmail.com>
2023-05-16 09:51:04 -0400

    20230516.01
    L'image du fond d'écran est également enregistrer dans le dossier "CachedFIle." En supprimant ce fichier, l'image de fond d'écran se met maintenant à jour avec la bonne image.

--- installWindows.ps1
+++ installWindows.ps1
@@ -422,6 +422,7 @@ function CleanDesktop {
         $wallpaperPath = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Themes"
         Remove-Item -Path "$wallpaperPath\TranscodedWallpaper"
         Copy-Item -Path .\extension\wallpaper.jpg -Destination "$wallpaperPath\TranscodedWallpaper"
+        Remove-Item -Path "$wallpaperPath\CachedFiles" -Recurse
     }
 
 }
--- test.ps1
+++ test.ps1
@@ -2,3 +2,4 @@
 $wallpaperPath = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Themes"
 Remove-Item -Path "$wallpaperPath\TranscodedWallpaper"
 Copy-Item -Path .\extension\wallpaper.jpg -Destination "$wallpaperPath\TranscodedWallpaper"
+Remove-Item -Path "$wallpaperPath\CachedFiles" -Recurse
