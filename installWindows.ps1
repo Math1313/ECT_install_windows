5d576df27d8c3604a345ccf8346997d7e8772ea9
Math1313 <mathisgauthier@hotmail.com>
2023-05-16 11:29:40 -0400

    20230516.02
    Configuration d'une nouvelle méthode, car il y a une erreur avec l'ancienne méthode

--- installWindows.ps1
+++ installWindows.ps1
@@ -419,11 +419,28 @@ function CleanDesktop {
     #Changement du fond d'écran si installation est SonXPlus
     if($installationType -eq 2)
     {
-        $wallpaperPath = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Themes"
-        Remove-Item -Path "$wallpaperPath\TranscodedWallpaper"
-        Copy-Item -Path .\extension\wallpaper.jpg -Destination "$wallpaperPath\TranscodedWallpaper"
-        Remove-Item -Path "$wallpaperPath\CachedFiles" -Recurse
-    }
+        $wallpaperPath = ".\extension\wallpaper.jpg"
+        $imagesPath = "$env:USERPROFILE\Pictures"
+        Copy-Item -Path "$wallpaperPath" -Destination "$imagesPath\wallpaper.jpg"
+
+        # Définit le chemin d'accès de l'image comme fond d'écran
+        Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "WallPaper" -Value "$imagesPath\wallpaper.jpg"
+
+        # Rafraîchit le fond d'écran
+$signature = @"
+[DllImport("user32.dll", CharSet = CharSet.Auto)]
+public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+"@
+        $systemParametersInfo = Add-Type -MemberDefinition $signature -Name WallpaperUtils -Namespace "Wallpaper" -PassThru
+        $result = $systemParametersInfo::SystemParametersInfo(0x0014, 0, "$imagesPath\wallpaper.jpg", 0x01)
+
+        # Vérifie le résultat
+        if ($result -eq 0) {
+                Write-Host "Échec du changement du fond d'écran."
+            } else {
+                Write-Host "Le fond d'écran a été changé avec succès."
+            }
+        }
 
 }
 # ------------------------------------------------------------------------------------------------------------------ #
