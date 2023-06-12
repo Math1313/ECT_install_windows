a3a8f6a7dfc75d3fb6a06ef8aaecb8068c82dda7
Math1313 <mathisgauthier@hotmail.com>
2023-06-12 13:01:37 -0400

    20230612.02

--- test_runningapp.ps1
+++
@@ -1,9 +0,0 @@
-$processList = Get-Process | ? { $_.MainWindowTitle } | Select-Object ProcessName
-
-if($processList.ProcessName -contains "Ninite")
-{
-    Write-Host "Balls"
-}
-else {
-    Write-Host "Done"
-}
